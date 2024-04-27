d8643cb914efd76b0504c2183aefd9f22fc86812
Mayotopia <134679840+Mayotopia04@users.noreply.github.com>
2024-04-27 21:00:02 +0800

    commit initial

---
+++ src/api.js
@@ -0,0 +1,22 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '70437a5dc1mshc25ce24f0787fccp13cba9jsn88113a73aaae',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+    },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8ee2ad7d143d493202cede870fde3e7b";
+
+
+
+
+// try {
+//     const response = await fetch(url, options);
+//     const result = await response.text();
+//     console.log(result);
+// } catch (error) {
+//     console.error(error);
+// }
